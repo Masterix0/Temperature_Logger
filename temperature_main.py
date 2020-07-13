ab1368029929805b323102ed28c07c19211b57cf
Masterix0 <andregoncalves2001@hotmail.com>
2020-07-13 17:37:03 +0100

    Added main cycle. Ignored logs folder in gitignore

--- temperature_main.py
+++ temperature_main.py
@@ -14,7 +14,7 @@ pol_time = temperature_config_reader.get_pol_time()
 
 # Initializes variables based on time
 
-today = datetime.date.today()
+current_day = datetime.date.today()
 
 
 # Checks whether the 'Temperature_Logs' directory exists
@@ -25,3 +25,24 @@ logs_path = current_path / 'Temperature_Logs'
 
 if not logs_path.exists():
     logs_path.mkdir()
+
+# Main body
+
+while True:
+    # Checks what day it is and, if it's a later date than the one
+    # we're working with, changes the current day
+    now = datetime.date.today()
+    if now > current_day:
+        current_day = now
+
+    # Gets the current temperature and time
+    current_temp = temperature_parser.get_temp()
+    current_time = datetime.datetime.now()
+
+    # Writes the new temperature reading to today's log
+    with open((logs_path / current_day.isoformat()), 'a') as todays_log:
+        todays_log.write('{hour}:{minutes}:{seconds}-{temperature}\n'.format(
+            hour=current_time.hour, minutes=current_time.minute,
+            seconds=current_time.second, temperature=current_temp))
+
+    time.sleep(pol_time)
