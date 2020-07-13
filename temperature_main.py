6a77edb7a3ee77e5e9f3a77a4e0921cdd805d9f8
Masterix0 <andregoncalves2001@hotmail.com>
2020-07-13 17:06:44 +0100

    Added the logs directory creation

--- temperature_main.py
+++ temperature_main.py
@@ -1,4 +1,27 @@
+# Import standart libraries
 import datetime
+import time
+import pathlib
+
+# Import custom libraries
 import temperature_parser
+import temperature_config_reader
+
+# Initializes variables from the config file
+
+pol_time = temperature_config_reader.get_pol_time()
+
+
+# Initializes variables based on time
 
 today = datetime.date.today()
+
+
+# Checks whether the 'Temperature_Logs' directory exists
+# and, if it doesn't, creates it
+
+current_path = pathlib.Path.cwd()
+logs_path = current_path / 'Temperature_Logs'
+
+if not logs_path.exists():
+    logs_path.mkdir()
