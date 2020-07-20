0e8c990851356c3ef7ca936044d9393e4b79dfd4
Masterix0 <andregoncalves2001@hotmail.com>
2020-07-20 18:52:39 +0100

    Added min and max temp and commented config initializer

--- temperature_config_initializer.py
+++ temperature_config_initializer.py
@@ -1,12 +1,16 @@
 import json
 
+# Welcome phrases
 print("Welcome to the Temperature Logger config initializer!")
 print("Please input the following deatils, so they can be written to the config file!")
+
+# General Info
 polling_interval = int(input(
     "Polling interval (delay between temperature reads, in seconds): "))
 emails_on = input(
     "Email usage (whether you want the program to email you with relevant information)\nYES or NO: ").lower()
 
+# General email info
 if emails_on == "yes":
     email_on = True
     email_username = input(
@@ -22,7 +26,7 @@ else:
     email_username, email_password = ""
     graph_on, temperature_alert_on = "False"
 
-
+# Graph email info
 if graph_on == "yes":
     graph_email_on = True
     graph_email_subject = input("Your graphs emails' subject: ")
@@ -35,8 +39,13 @@ else:
     graph_email_subject, graph_email_body, graph_email_receiver = ''
     graph_email_on = False
 
+# Alert email info
 if temperature_alert_on == "yes":
     alert_email_on = True
+    alert_min_temp = float(input(
+        "The LOWEST temp acceptable before getting an alert: "))
+    alert_max_temp = float(input(
+        "The HIGHEST temp acceptable before getting an alert: "))
     alert_email_subject = input("Your alerts emails' subject: ")
     alert_email_body = input(
         "Your alert email body (written in string format)\nPLEASE use '{temperature}' to signal where you\nwant your temperature to go, as it will be used to format the string: ")
@@ -44,27 +53,38 @@ if temperature_alert_on == "yes":
         "Email address which will receive the alerts: ")
 
 else:
-    alert_email_subject, alert_email_body, alert_email_receiver = ''
+    alert_email_subject, alert_email_body, alert_email_receiver, alert_min_temp, alert_max_temp = ''
     alert_email_on = False
 
+# Create a dictionary using all the inputs
+# This is a standart config format, seeing that the actual
+# JSON file will be very similar
+# Standard values are below
 json_config = {
-    "polling_interval": polling_interval,
-    "email_on": email_on,
-    "email_username": email_username,
-    "email_password": email_password,
+    "polling_interval": polling_interval,  # 300 (5 minutes)
+    "email_on": email_on,  # True
+    "email_username": email_username,  # blabla@gmail.com
+    "email_password": email_password,  # xxxxxxxxxxxxxxxxxxx
     "graph_email": {
-        "graph_email_on": graph_email_on,
-        "graph_email_subject": graph_email_subject,
+        "graph_email_on": graph_email_on,  # True
+        "graph_email_subject": graph_email_subject,  # Yesterday's temperature data
+        # "A random message\n\nBest regards,\nBlabla Bot"
         "graph_email_body": graph_email_body,
-        "graph_email_receiver": graph_email_receiver
+        "graph_email_receiver": graph_email_receiver  # bleble@gmail.com
     },
     "temperature_alert_email": {
-        "alert_email_on": alert_email_on,
-        "alert_email_subject": alert_email_subject,
+        "alert_email_on": alert_email_on,  # True
+        "alert_min_temp": alert_min_temp,  # 16.0
+        "alert_max_temp": alert_max_temp,  # 26.0
+        "alert_email_subject": alert_email_subject,  # OOPS, Temperature Exceeded Limit
         "alert_email_body": alert_email_body,
-        "alert_email_receiver": alert_email_receiver
+        # "\"This email serves as a notification that your beer is currently
+        # at {temperature} degrees.\\nToday's temperature graph is sent as
+        # an attachment.\\n\\nBest regards,\\nBot Cervejeiro.\""
+        "alert_email_receiver": alert_email_receiver  # bleble@gmail.com
     }
 }
 
+# Save values to config file
 with open("config.json", 'w') as config:
     json.dump(json_config, config)
